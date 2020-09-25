7b42403bfd092f57ab4cdf90ae3745c0523c9e5e
unknown <konstantintaratorkin@mail.ru>
2020-09-25 15:44:37 +0300

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from './Header';
+import './css/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header></Header>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './css/Header.css';
+
+function Header() {
+  return (
+    <div className="Header">
+      Hello
+    </div>
+  );
+}
+
+export default Header;
